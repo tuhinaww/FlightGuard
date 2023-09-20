9ff0bd597e1666c66d30f12712a9c1ac5502816f
tuhinaww <iceybubblegumtea@gmail.com>
2023-09-20 21:45:23 +0530

    deleted:    frontend/src/App.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home, Search } from "./pages";
 
-import "./App.css";
-
 const App = () => {
   return (
     <ChakraProvider>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="search" element={<Search />} />
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="search" element={<Search />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </ChakraProvider>

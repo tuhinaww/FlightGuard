3eca1e7a1ac8481614793237f89e25efec017798
tuhinaww <iceybubblegumtea@gmail.com>
2023-09-20 22:33:00 +0530

    modified:   frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { createTheme, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home, Search } from "./pages";
-
-const theme = createTheme({
-
-
-
-});
 
 const App = () => {
   return (
     <ChakraProvider>
-      <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
@@ -21,7 +13,6 @@ const App = () => {
           </Route>
         </Routes>
       </BrowserRouter>
-      </ThemeProvider>
     </ChakraProvider>
   );
 };
